94bbdca06f4263103b6945a4a07fa6f5721873cc
leejunhee <z6z6jun@naver.com>
2021-07-05 02:18:50 +0900

    프로그래머스 Lv1 약수의합

---
+++ BaekJoonTest/src/ProgrammersTest/Test4.java
@@ -0,0 +1,19 @@
+package ProgrammersTest;
+
+
+//약수의 
+public class Test4 {
+	public static void main(String[] args) {
+		
+		int n = 12;
+		int answer = 0;
+		
+		for(int i = 1; i<= n; i++) {
+			if(n%i ==0) {
+				answer += i;
+			}
+		}
+	
+		System.out.println(answer);
+	}
+}
