5d0877a18b7881c539abf4e61f33123dc4ce8553
leejunheee <z6z6jun@naver.com>
2021-10-01 23:02:17 +0900

    정수 제곱근 판별

---
+++ BaekJoonTest/src/ProgrammersTest/Test19.java
@@ -0,0 +1,25 @@
+package ProgrammersTest;
+
+//정수 제곱근 판별
+public class Test19 {
+	public static void main(String[] args) {
+		Solution20 s = new Solution20();
+		System.out.println(s.solution(9));
+
+	}
+}
+
+class Solution20 {
+	public long solution(long n) {
+		
+		Double sqrt = Math.sqrt(n);
+		
+		if(sqrt == sqrt.intValue()) {
+			return (long)Math.pow(sqrt +1, 2);
+		} else {
+			return -1;
+		}
+
+		
+	}
+}
