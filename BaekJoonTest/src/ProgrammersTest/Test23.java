7b15b293012002dbbd807df94072405a92c8283a
leejunhee <z6z6jun@naver.com>
2021-10-05 22:39:27 +0900

    음양 더하기

---
+++ BaekJoonTest/src/ProgrammersTest/Test23.java
@@ -0,0 +1,35 @@
+package ProgrammersTest;
+
+//음양 더하기
+
+public class Test23 {
+	public static void main(String[] args) {
+		Solution24 s = new Solution24();
+		int[] a = { 4, 7, 12 };
+		boolean[] b = { true, false, true };
+
+		System.out.println(s.solution(a, b));
+
+	}
+}
+
+class Solution24 {
+	public int solution(int[] absolutes, boolean[] signs) {
+		int answer = 0;
+
+		for (int i = 0; i < absolutes.length; i++) {
+			if (signs[i]) {
+				answer += absolutes[i];
+			} else {
+				answer -= absolutes[i];
+			}
+		}
+		
+//		for(int i =0 ; i< absolutes.length; i++) {
+//			answer += absolutes[i] * (signs[i] ? 1 : -1);
+//		}
+
+
+		return answer;
+	}
+}
